e82219dae57980a536e4fdbfbdbaa33aab9f5ee8
Hildermes José Medeiros Filho <hildermes@gmail.com>
2024-03-09 05:34:32 -0300

    Update start.sh
    final start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
 
-# Check if the Tomcat server has already been created
-if [ ! -f "/usr/local/tomcat/server-created.flag" ]; then
-    echo "Creating the Tomcat Server site..."
-    # Ensure HOSTNAME is provided
-    if [ -z "$HOSTNAME" ]; then
-        echo "HOSTNAME environment variable is not set."
+# Check if the Apache2 server has already been configured
+if [ ! -f "/etc/apache2/server-created.flag" ]; then
+    echo "Creating the Apache2-tomcat Server site..."
+
+    export LOWERCASE_HOSTNAME=$(echo ${HOSTNAME} | tr '[:upper:]' '[:lower:]')
+    export UPPERCASE_HOSTNAME=$(echo ${HOSTNAME} | tr '[:lower:]' '[:upper:]')
+    # Apache2 confs
+    
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout /etc/ssl/private/apache-selfsigned.key \
+    -out /etc/ssl/certs/apache-selfsigned.crt \
+    -subj "/OU=Self Signed Certificate/CN=${HOSTNAME}" \
+    -addext "subjectAltName = DNS:${LOWERCASE_HOSTNAME},DNS:${UPPERCASE_HOSTNAME}"
+    
+    # Check if custom site configuration exists and move it
+    if [ -f "/etc/apache2/my-apache-site.conf" ]; then
+        mv /etc/apache2/my-apache-site.conf /etc/apache2/sites-available/${HOSTNAME}.conf
+        a2ensite ${HOSTNAME}.conf
+    else
+        echo "/etc/apache2/my-apache-site.conf does not exist."
         exit 1
     fi
 
-    # Convert the hostname to lowercase and uppercase
-    export LOWERCASE_HOSTNAME=$(echo ${HOSTNAME} | tr '[:upper:]' '[:lower:]')
-    export UPPERCASE_HOSTNAME=$(echo ${HOSTNAME} | tr '[:lower:]' '[:upper:]')
-
+    # Disable default site and enable necessary modules
+    a2dissite 000-default.conf
+    a2enmod ssl
+    a2enmod headers
+    a2enmod socache_shmcb
+    a2enmod rewrite
+    a2enmod proxy proxy_http proxy_ajp proxy_connect
+    # Add ServerName to apache2.conf
+    echo "ServerName ${HOSTNAME}" | tee -a /etc/apache2/apache2.conf
+    # Check the configuration for errors
+    apache2ctl configtest
+    service apache2 stop
+    # Tomcat confs
     # Generate a keystore for Tomcat
     keytool -genkeypair \
         -alias "${TOMCAT_KEY_ALIAS}" \
@@ -24,7 +47,6 @@ if [ ! -f "/usr/local/tomcat/server-created.flag" ]; then
         -keypass "${TOMCAT_KEY_PASSWORD}" \
         -dname "CN=${HOSTNAME}, OU=Self Signed Certificate" \
         -ext "SAN=dns:${LOWERCASE_HOSTNAME},dns:${UPPERCASE_HOSTNAME}"
-
     # Create tomcat-users.xml
     cat << EOF > /usr/local/tomcat/conf/tomcat-users.xml
 <?xml version="1.0" encoding="UTF-8"?>
@@ -37,13 +59,14 @@ if [ ! -f "/usr/local/tomcat/server-created.flag" ]; then
     <user username="${TOMCAT_USERNAME}" password="${TOMCAT_PASSWORD}" roles="manager-gui,manager-script"/>
 </tomcat-users>
 EOF
-sed -i "s/\${KEYSTORE_PLACEHOLDER}/${TOMCAT_KEY_PASSWORD}/g" /usr/local/tomcat/conf/server.xml
-sed -i "s/\${ALIAS_PLACEHOLDER}/${TOMCAT_KEY_ALIAS}/g" /usr/local/tomcat/conf/server.xml
-    # Mark the server as created by creating the flag file
-    echo "Tomcat server setup completed on $(date)" > "/usr/local/tomcat/server-created.flag"
+    sed -i "s/\${KEYSTORE_PLACEHOLDER}/${TOMCAT_KEY_PASSWORD}/g" /usr/local/tomcat/conf/server.xml
+    sed -i "s/\${ALIAS_PLACEHOLDER}/${TOMCAT_KEY_ALIAS}/g" /usr/local/tomcat/conf/server.xml
+    echo "Apache2-tomcat server setup completed on $(date)" > "/etc/apache2/server-created.flag"
+
 else
-    echo "Tomcat site already created. Starting Server"
+    echo "Apache2-tomcat site already created. Starting Server"
 fi
 
-# Execute the main container command
-exec /usr/local/tomcat/bin/catalina.sh run
+# Start Apache in the foreground
+gosu tomcat /usr/local/tomcat/bin/catalina.sh run &
+exec apachectl -DFOREGROUND
